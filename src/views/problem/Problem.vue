fd7543bf64e926092903bce8c48f83e9728bd2ce
Nekocatso <3093490690@qq.com>
2024-04-21 21:26:54 +0800

    更改几个problem相关的小差错

--- src/views/problem/Problem.vue
+++ src/views/problem/Problem.vue
@@ -3,8 +3,18 @@
 </template>
 
 <script lang="ts" setup>
-
-</script>
+    import Vue from 'vue'
+    import mavonEditor from 'mavon-editor'
+    import 'mavon-editor/dist/css/index.css'
+    // use
+    Vue.use(mavonEditor)
+    new Vue({
+        'el': '#main',
+        data() {
+            return { value: '' }
+        }
+    })
+</script>     
 
 <style scoped>
     
